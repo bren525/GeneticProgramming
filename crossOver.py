74923c29f94b2817cf214b3c4cb7fa3b45180f93
cbailey1 <cbailey1@ubuntu.(none)>
2013-05-02 16:22:58 -0400

    Added to crossOver

--- crossOver.py
+++ crossOver.py
@@ -1,3 +1,18 @@
-def crossOver(mom,dad):
+import random
+
+def crossOver(mom,dad,count):
 	"""Takes in two genomes swaps their elements and produces two babies"""
+	
+	cut = random.randint(1, len(mom)-1)
+	
+	if count >= 1:
+		sis=mom.clone()
+		sis[cut:]=dad[cut:]
+	if count == 2:
+		bro=dad.clone()
+		bro[cut:]=dad[cut:]
+	
 return bro,sis
+
+if __name__ == '__main__':
+	print(crossOver([1,3,5,8],[2,4,7,9],2))
