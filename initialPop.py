e2e83fb4f17a1ea705f2179a345eb981bd28c231
cbailey1 <cbailey1@ubuntu.(none)>
2013-05-02 16:51:45 -0400

    wrote initializator

--- initialPop.py
+++ initialPop.py
@@ -1,3 +1,19 @@
-def initialPop(popSize,minRange,maxRange,genLength):
+import random
 
+def initialPop(popSize,rangeMin,rangeMax,genLength):
+	"""Creates the initial population for evolving"""
+	
+	pop=[]
+	genome=[]
+
+	for i in range(popSize):
+		for j in range(genLength):
+			param=random.randint(rangeMin,rangeMax)
+			genome.append(param)
+		pop.append(genome)
+				
 	return pop
+
+
+if __name__=='__main__':
+	print(initialPop(10,1,12,4))
