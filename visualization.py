eb47ba71d92cc4f9ad3da952693c1874a6f30228
Grace Ahn <saewonahn@ubuntu.(none)>
2013-05-08 00:51:35 -0400

    merging

--- visualization.py
+++ visualization.py
@@ -4,15 +4,16 @@ import time
 
 def plotting(results):
     fig = plt.figure(figsize=(8,6))
-    plt.ylim((-1000,1000))
-    plt.xlim((-1000,1000))
+    plt.ylim((-500,500))
+    plt.xlim((-500,500))
     plt.ion()
     for i in range(0, len(results)):   
-        plt.plot(results[i][0],results[i][1],'ro',markersize=2)
-        plt.plot(results[i][2],results[i][3],'bo',markersize=2)
-        print ('plot work!')
+        plt.plot(results[i][0],results[i][1],'ro',markersize=4)
+        plt.plot(results[i][2],results[i][3],'bo',markersize=4)
+        # print ('plot work!')
         plt.draw()
         time.sleep(0.1)
+    plt.show(20)    
 
 if __name__ == '__main__':
 
